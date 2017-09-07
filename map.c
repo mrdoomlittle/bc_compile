fe49554420e5859604115a10158a97d718ed8dba
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-09-07 20:44:23 +0100

    .

--- map.c
+++ map.c
@@ -20,7 +20,8 @@ mdl_u32_t map_hash(mdl_u8_t const *__key, mdl_uint_t __bc) {
 	mdl_u8_t const *itr = __key;
 	mdl_u32_t ret_val = 1;
 	while(itr != __key+__bc) {
-		ret_val = ((ret_val>>(*itr&3))+(*itr*((itr-__key)+128)));
+		ret_val = (((ret_val>>1)+1)*(ret_val>>1))+ret_val+((*itr*((itr-__key)+1))*(*itr>>1));
+		ret_val = ((ret_val+128)^128)*((ret_val>>1)+1);
 		itr++;
 	}
 
